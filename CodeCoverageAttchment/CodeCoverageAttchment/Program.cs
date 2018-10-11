1a1747e31e5c8eeca0a3d1fa4013f2f0d5de43eb
siddhap <siddhap@microsoft.com>
2018-10-11 13:55:13 +0530

    chnages to ensure we can use new log tore also

--- CodeCoverageAttchment/CodeCoverageAttchment/Program.cs
+++ CodeCoverageAttchment/CodeCoverageAttchment/Program.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.TeamFoundation.Client;
-using Microsoft.TeamFoundation.TestManagement.WebApi;
-using Microsoft.VisualStudio.Services.Client;
-using Microsoft.VisualStudio.Services.Common;
-using Microsoft.VisualStudio.Services.WebApi;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.TeamFoundation.TestClient.PublishTestResults;
+using Microsoft.TeamFoundation.TestManagement.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
 
 namespace CodeCoverageAttchment
 {
@@ -14,28 +13,83 @@ namespace CodeCoverageAttchment
     {
         static void Main(string[] args)
         {
-            //System.Diagnostics.Debugger.Launch();
-            string collectionUrl = args[0];
+            try
+            {
+                //System.Diagnostics.Debugger.Launch();
+                string collectionUrl = args[0]; // $(System.TeamFoundationCollectionUri)
+                var connection = new VssConnection(new Uri(collectionUrl), new Microsoft.VisualStudio.Services.OAuth.VssOAuthAccessTokenCredential(args[4]));
 
-            var testClient = new TestManagementHttpClient(new Uri(collectionUrl), new Microsoft.VisualStudio.Services.OAuth.VssOAuthAccessTokenCredential(args[4]));
+                var testClient = new TestManagementHttpClient(new Uri(collectionUrl), new Microsoft.VisualStudio.Services.OAuth.VssOAuthAccessTokenCredential(args[4]));
 
-            string buildurl = args[1];
-            string projectName = args[2];
-            string folder = args[3];
+                string buildurl = args[1]; // $(BUILD.BUILDURI)
+                string projectName = args[2]; // $(SYSTEM.TEAMPROJECT)
+                string folder = args[3];
+                int buildId = int.Parse(args[5]); //$(Build.BuildId)
+                bool.TryParse(args[6], out bool useNewStore);
+                string projectId = args[7]; //$(System.TeamProjectId)
 
-            // Create test run
-            RunCreateModel runCreationModel = new RunCreateModel("somename", buildId: int.Parse(args[5]),
-                startedDate: DateTime.UtcNow.ToString(), completedDate: DateTime.UtcNow.ToString(),
-                state: TestRunState.InProgress.ToString(), isAutomated: true);
-            var testRun = testClient.CreateTestRunAsync(runCreationModel, projectName).Result;
+                // Create test run
+                RunCreateModel runCreationModel = new RunCreateModel("somename", buildId: buildId,
+                    startedDate: DateTime.UtcNow.ToString(), completedDate: DateTime.UtcNow.ToString(),
+                    state: TestRunState.InProgress.ToString(), isAutomated: true);
+                var testRun = testClient.CreateTestRunAsync(runCreationModel, projectName).Result;
 
+                TestCaseResult result = new TestCaseResult
+                {
+                    AutomatedTestName = "TestOne",
+                    AutomatedTestStorage = "foo.dll",
+                    Build = new ShallowReference { Url = buildurl },
+                    TestCaseTitle = "TestOne"
+                };
+                testClient.AddTestResultsToTestRunAsync(new TestCaseResult[] { result }, projectName, testRun.Id).Wait();
+                testClient.UpdateTestRunAsync(new RunUpdateModel("somename", state: TestRunState.Completed.ToString()), projectName, testRun.Id).Wait();
 
+                if (useNewStore)
+                {
+                    UploadCodeCoverageAttachmentsToNewStore(folder, connection, buildId, projectId);
+                }
+                else
+                {
+                    UploadCodeCoverageAttachmentsToOldStore(testClient, buildurl, projectName, folder);
+                }
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e);
+            }
+        }
 
-            TestCaseResult result = new TestCaseResult { AutomatedTestName = "TestOne", AutomatedTestStorage = "foo.dll",
-                Build = new ShallowReference { Url = buildurl }, TestCaseTitle = "TestOne" };
-            testClient.AddTestResultsToTestRunAsync(new TestCaseResult[] { result }, projectName, testRun.Id).Wait();
-            testClient.UpdateTestRunAsync(new RunUpdateModel("somename", state: TestRunState.Completed.ToString()), projectName, testRun.Id).Wait();
+        private static void UploadCodeCoverageAttachmentsToNewStore(string folder, VssConnection connection, int buildId, string projectId)
+        {
+            TestLogStore logStore = new TestLogStore(connection, new LogTraceListener());
+            Dictionary<string, string> files = new Dictionary<string, string>();
 
+            foreach (var file in Directory.EnumerateFiles(folder, "*.coverage", SearchOption.AllDirectories))
+            {
+                if (files.TryGetValue(Path.GetFileName(file), out string something))
+                {
+                    continue;
+                }
+                else
+                {
+                    files.Add(Path.GetFileName(file), file);
+                    Dictionary<string, string> metaData = new Dictionary<string, string>();
+                    metaData.Add("ModuleName", Path.GetFileName(file));
+                    var attachment = logStore.UploadTestBuildLogAsync(new Guid(projectId), buildId, Microsoft.TeamFoundation.TestManagement.WebApi.TestLogType.Intermediate, file, metaData, null, false, System.Threading.CancellationToken.None).Result;
+                }
+            }
+
+            foreach (var file in Directory.EnumerateFiles(folder, "*.coveragebuffer", SearchOption.AllDirectories))
+            {
+                Dictionary<string, string> metaData = new Dictionary<string, string>();
+                metaData.Add("ModuleName", Path.GetFileName(file));
+                var attachment = logStore.UploadTestBuildLogAsync(new Guid(projectId), buildId, Microsoft.TeamFoundation.TestManagement.WebApi.TestLogType.Intermediate, file, metaData, null, true, System.Threading.CancellationToken.None).Result;
+            }
+        }
+
+        private static void UploadCodeCoverageAttachmentsToOldStore(TestManagementHttpClient testClient, string buildurl, string projectName, string folder)
+        {
             var testRuns = testClient.GetTestRunsAsync(projectName, buildurl).Result;
 
             foreach (var t in testRuns)
@@ -93,16 +147,34 @@ namespace CodeCoverageAttchment
 
             return null;
         }
+    }
 
-        private static VssConnection CreateServerConnection()
+    public class LogTraceListener : TraceListener
+    {
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
         {
-            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(TfsTeamProjectCollection.GetFullyQualifiedUriForName("http://localhost:8080/tfs/Defaultcollection"));
-            VssCredentials credentials = new VssClientCredentials(useDefaultCredentials: true);
-            credentials.Storage = new VssClientCredentialStorage();
-            credentials.PromptType = CredentialPromptType.PromptIfNeeded;
-            VssConnection connection = new VssConnection(tfs.Uri, credentials);
+            switch (eventType)
+            {
+                case TraceEventType.Information:
+                    Console.WriteLine(message);
+                    break;
+                case TraceEventType.Warning:
+                    Console.WriteLine(message);
+                    break;
+                case TraceEventType.Verbose:
+                    Console.WriteLine(message);
+                    break;
+            }
+        }
 
-            return connection;
+        public override void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
