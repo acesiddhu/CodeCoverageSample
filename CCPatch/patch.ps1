c5b7804d35202e76ddf84b9297e76c7748cd8ea7
Karan Jit Singh <karsin@microsoft.com>
2019-03-25 11:16:32 +0530

    patch nit

--- CCPatch/patch.ps1
+++ CCPatch/patch.ps1
@@ -13,6 +13,10 @@ $authHeader = ConvertTo-BasicAuthHeader $env:SYSTEM_ACCESSTOKEN
 
 $projectId = $env:SYSTEM_TEAMPROJECTID
 
+# Sample links
+# https://vstmr.codedev.ms/foo/0e804a94-5253-4fb8-a183-7c79420142ff/_apis/testresults/CodeCoverage/?buildId=1234&api-version=5.0-preview.1
+# https://tcm1.dev.azure.com/foo/0e804a94-5253-4fb8-a183-7c79420142ff/_apis/testresults/CodeCoverage/?buildId=1234&api-version=5.0-preview.1
+
 $uri = $env:TCMADDRESS + '/' + $projectId + '/_apis/testresults/CodeCoverage/?buildId=' + $env:BUILD_BUILDID + '&api-version=5.0-preview.1'
 
 Write-Host 'Calling patch via ' $uri
@@ -22,6 +26,8 @@ $patchRequest = Invoke-WebRequest -Uri $uri -Headers $authHeader -Method Patch -
 try
 {
     Invoke-WebRequest -Uri $uri -Headers $authHeader -Method Patch -ContentType 'application/json'
+    Write-Host 'Request should have thrown an exception'
+    exit 1
 }
 catch
 {
